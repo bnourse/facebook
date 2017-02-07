789ab1fed7fb13976922cf18af14fd4f82e7b864
Ben Nourse <bnourse@gmail.com>
2017-02-07 14:32:05 -0600

    added cuntionality

--- scripts.js
+++ scripts.js
@@ -8,7 +8,19 @@ window.addEventListener("load", function (){
 
 	mediaInfos = document.getElementsByClassName("media__info")
 	for(i = 0; i < mediaInfos.length; i++) {
-		mediaInfos[i].childNodes[1].addEventListener("click", nameLinkClicked);
+		//Make sure this is a name link, not a comment box
+		aTags = mediaInfos[i].getElementsByTagName("a");
+		if (aTags[0] != null) {
+			//first element is an a tag containing Name
+			aTags[0].addEventListener("click", nameLinkClicked);
+		}
+		else {
+			//first element is the comment box, not sure if it's the text element or a wrapper around it
+			replyForm = mediaInfos[i].getElementsByTagName("form")[0];
+			if (replyForm != null) {
+				replyForm.addEventListener("submit", replySubmitted);
+			}
+		}
 	}
 
 	modalClose = document.getElementsByClassName("modal__close")[0];
@@ -16,8 +28,14 @@ window.addEventListener("load", function (){
 
 	modalBackground = document.getElementsByClassName("modal")[0];
 	modalBackground.addEventListener("click", modalBackgroundClicked);
+
+	postCommentLink = document.getElementsByClassName("action--comment")[0];
+	postCommentLink.addEventListener("click", postCommentLinkClicked);
 });
 
+function postCommentLinkClicked() {
+	document.getElementsByClassName("post__details")[0].childNodes[5].childNodes[3].childNodes[1].childNodes[1].focus()
+}
 
 function likeClicked() {
 	likeCountString = this.parentNode.childNodes[5].textContent;
@@ -36,14 +54,17 @@ function likeClicked() {
 
 function nameLinkClicked() {
 	document.getElementsByClassName("modal")[0].style.display = "block";
+
 }
 
 function modalCloseClicked() {
 	document.getElementsByClassName("modal")[0].style.display = "none";
 }
 
-function modalBackgroundClicked() {
-	document.getElementsByClassName("modal")[0].style.display = "none";
+function modalBackgroundClicked(e) {
+	if (e.target == this) {
+		document.getElementsByClassName("modal")[0].style.display = "none";
+	}
 }
 
 function replyLinkClicked() {
@@ -61,3 +82,18 @@ function replyLinkClicked() {
 	}
 }
 
+function replySubmitted(e) {
+	content = this.childNodes[1].value
+	if(content.trim() == ""){
+		alert("Can't submit an empty reply");
+	}
+	else {
+		addReply(content);
+		this.childNodes[1].value = "";
+	}
+	e.preventDefault();
+}
+
+function addReply(content) {
+	
+}
