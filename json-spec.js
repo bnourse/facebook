de864f250b81b8bd1bf388c5034b66f800181552
Ben Nourse <bnourse@gmail.com>
2017-02-15 21:58:05 -0600

    added theoretical json spec

---
+++ json-spec.js
@@ -0,0 +1,27 @@
+{
+	"postID": 12345
+	"authorID": 67890
+	"likeCount": 28
+	"postContent": "Hello world!"
+	"timestamp": "Yesterday at 10:00am"
+	"replyIDs": [
+		11111,
+		22222,
+		33333
+	]
+
+}
+
+{
+	"ID": 12345
+	"firstname": "Joe"
+	"lastname": "Smith"
+	"profilePic": "/profileImage/12345.png"
+	"friends": [
+		11111,
+		22222,
+		33333,
+		44444,
+		55555
+	]
+}
