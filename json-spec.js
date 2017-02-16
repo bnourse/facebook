f4653f70a35ce2c9243d24c0fe206cebf7f05724
Ben Nourse <bnourse@gmail.com>
2017-02-16 09:20:52 -0600

    validated

--- json-spec.js
+++ json-spec.js
@@ -1,9 +1,9 @@
 {
-	"postID": 12345
-	"authorID": 67890
-	"likeCount": 28
-	"postContent": "Hello world!"
-	"timestamp": "Yesterday at 10:00am"
+	"postID": 12345,
+	"authorID": 67890,
+	"likeCount": 28,
+	"postContent": "Hello world!",
+	"timestamp": "Yesterday at 10:00am",
 	"replyIDs": [
 		11111,
 		22222,
@@ -13,10 +13,10 @@
 }
 
 {
-	"ID": 12345
-	"firstname": "Joe"
-	"lastname": "Smith"
-	"profilePic": "/profileImage/12345.png"
+	"ID": 12345,
+	"firstname": "Joe",
+	"lastname": "Smith",
+	"profilePic": "/profileImage/12345.png",
 	"friends": [
 		11111,
 		22222,
